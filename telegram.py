0a24af18f1f4f7e38a7b357090a5c74bb9ecb671
MrOrangeUS <71477247+MrOrangeUS@users.noreply.github.com>
2025-05-20 04:18:19 -0500

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -219,7 +219,7 @@ else:
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
         payload = {"chat_id": chat_id, "text": reply}
         requests.post(url, json=payload)
-        return "OK", 200
+        # return "OK", 200
 
     except Exception as e:
         logger.error(f"Webhook error: {e}")
